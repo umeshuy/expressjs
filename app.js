90467b7357aaab56c759269b9375becd6cf12112
Umesh Yadav <110802360+umeshuy@users.noreply.github.com>
2023-01-04 16:46:42 +0530

    adding form on add product page

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the Middleware');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res,next)=>{
+    res.send('<form action="/product" method="POST"><input type = "text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next)=>{
-    console.log('In the another Middleware');
+app.use('/product',(req,res,next)=>{
+
+    res.redirect('/');
+});
+
+app.use((req, res, next)=>{ 
     res.send('<h1>Hello From Express!</h1>')
 })
 
